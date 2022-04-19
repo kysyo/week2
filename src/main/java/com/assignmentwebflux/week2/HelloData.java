13a788ec4415dbe05cd601f1711cfe7bc308915f
[지나웍스] 김영수 <kysyo@zinnaworks.com>
2022-04-19 12:04:59 +0900

    2주차 과제 - REST API에 반응하는 local웹 서버 앱 구축

--- src/main/java/com/assignmentwebflux/week2/HelloData.java
+++ src/main/java/com/assignmentwebflux/week2/HelloData.java
@@ -12,4 +12,21 @@ public class HelloData {
         this.to = to;
         this.message = message;
     }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
